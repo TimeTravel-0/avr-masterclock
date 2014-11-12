96b3b85b0468b520feb843c5441d9766c13a7f1c
TimeTravel-0 <TimeTraveller0@users.noreply.github.com>
2014-11-12 22:55:51 +0100

    corrected export statement

--- setup_mfile.sh
+++ setup_mfile.sh
@@ -3,8 +3,7 @@ sudo apt-get install tcl8.5 tk8.5 expect
 wget http://www.sax.de/~joerg/mfile/mfile.tar.gz
 tar -xzf mfile.tar.gz 
 rm mfile.tar.gz
-export MFILE_HOME="."
 cd mfile
-echo "wish mfile.tcl" > run.sh
+echo "export MFILE_HOME="." ; wish mfile.tcl" > run.sh
 chmod +x run.sh
 ./run.sh
